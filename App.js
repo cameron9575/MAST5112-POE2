5326103cdc10a78b7c09c34b35de6540de004724
cameron9575 <122437299+cameron9575@users.noreply.github.com>
2024-10-08 18:16:09 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Menu from "./menu";
+import Categories from "./categories";
+import items from "./data";
+import logo from "./logo.jpeg";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(items);
+  const [activeCategory, setActiveCategory] = useState("");
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <img src={logo} alt="logo" className="logo" />
+          <h2> Chef's Menu List</h2>
+          <div className="underline"></div>
+          <p>The Chef has prepared 3 meals for each cousre </p>
+        </div>
+        <Categories
+          categories={categories}
+          activeCategory={activeCategory}
+          filterItems={filterItems}
+        />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
+
+export default App;
